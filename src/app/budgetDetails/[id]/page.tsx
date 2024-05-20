fb1aeed3ba4660742c02074ed6fb55b9f1237304
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-05-20 16:08:53 +0530

    dataFetching 8/10

--- src/app/budgetDetails/[id]/page.tsx
+++ src/app/budgetDetails/[id]/page.tsx
@@ -7,8 +7,6 @@ type Params = {
 };
 
 export default async function page({ params: { ID } }: Params) {
-  console.log("Fetching data for ID:", ID); // Debugging log
-  
   async function fetchTotalRelatedRecordAmount() {
     const response = await fetch(
       `http://localhost:5000/api/v1/totalRelatedRecordAmount/${ID}`,
@@ -24,13 +22,41 @@ export default async function page({ params: { ID } }: Params) {
     return response.json();
   }
 
-  const data = await fetchTotalRelatedRecordAmount();
-  const total = data["Total"];
+  async function fetchAllBudgetRelatedExpenseRecords() {
+    const response = await fetch(
+      `http://localhost:5000/api/v1/relatedExpenseRecord/${ID}`,
+      {
+        cache: "no-cache",
+      }
+    );
+
+    if (!response.ok) {
+      console.log(Error);
+      toast.error("Failed to fetch related records");
+    }
+    return response.json();
+  }
+
+  const totalRelatedRecordAmountData = await fetchTotalRelatedRecordAmount();
+  const total = totalRelatedRecordAmountData["Total"];
+
+  const allBudgetRelatedExpenseRecords =
+    await fetchAllBudgetRelatedExpenseRecords();
+  const relatedExpenseRecords =
+    allBudgetRelatedExpenseRecords["All related records"];
 
   return (
     <div>
       <h1>This is the total</h1>
       <h2>{total}</h2>
+
+      {relatedExpenseRecords.map((records: any) => (
+        <ul key={records.ID}>
+          <li>
+            {records.RelatedBudgetID} || {records.RelatedExpenseID}
+          </li>
+        </ul>
+      ))}
     </div>
   );
 }
